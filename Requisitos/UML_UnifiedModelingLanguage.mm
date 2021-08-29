4a136fb820b269e11db287c4606ff9ac767a5cf5
Marcondes Cunha Araujo <marcunharaujo@gmail.com>
2021-08-29 19:28:28 -0300

    UML

---
+++ Requisitos/UML_UnifiedModelingLanguage.mm
@@ -0,0 +1,142 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1629582676638" ID="ID_357676166" LINK="https://www.uml.org/" MODIFIED="1629587102615">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      UML
+    </p>
+    <p style="text-align: center">
+      <font color="rgb(32, 33, 36)" face="Google Sans, arial, sans-serif">Unified Modeling Language</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1629583278321" ID="ID_1023919871" MODIFIED="1629587102513" POSITION="left" TEXT="Implanta&#xe7;&#xe3;o da Solu&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#00b439" CREATED="1629583308576" ID="ID_1143125526" MODIFIED="1629587102516" TEXT="Diagrama de Estados">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629585136151" ID="ID_1721992745" MODIFIED="1629587102516" TEXT="&#xc9; um diagrama din&#xe2;mico que representa os estados de objetos no sistema.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629583323084" ID="ID_517828960" MODIFIED="1629587102521" TEXT="Diagrama de Companentes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629585313417" ID="ID_883408772" MODIFIED="1629587102521" TEXT="&#xc9; um diagrama est&#xe1;tico que modela o sistema em termos de artefatos e seus &#xa;relacionamentos por meio de interfaces. Permite decompor um sistema em subsistemas.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629585027018" ID="ID_654457245" MODIFIED="1629587102528" TEXT="Encapsulamento">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629585051091" ID="ID_991351523" MODIFIED="1629587102533" TEXT="Torna poss&#xed;vel o agrupamento de atributos e opera&#xe7;&#xf5;es do objeto, facilitando o acesso a estas informa&#xe7;&#xf5;es mesmo que o requisitante n&#xe3;o conhe&#xe7;a detalhes da implementa&#xe7;&#xe3;o do objeto.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1629583058914" ID="ID_54980318" MODIFIED="1629587102543" POSITION="right" TEXT="An&#xe1;lise de Requisitos">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-1"/>
+<node COLOR="#00b439" CREATED="1629583220816" ID="ID_1067103060" MODIFIED="1629587102543" TEXT="Diagrama de Classes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629583698399" ID="ID_443332330" MODIFIED="1629587102543" TEXT="Representa a estrutura, ou seja, as classes; entidades que v&#xe3;o compor o meu modelo, considerando seus atributos, suas opera&#xe7;&#xf5;es.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629583234512" ID="ID_621184102" MODIFIED="1629587102549" TEXT="Diagrama de Caso de Uso">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629583893145" ID="ID_699906083" MODIFIED="1629587102549" TEXT="&#xc9; um diagrama din&#xe2;mico que &#xe9; usado para modelar as intera&#xe7;&#xf5;es entre os atores e/ou com o sistema descrevendo uma s&#xe9;rie de requisitos num ambiente chamado &quot;cen&#xe1;rio&quot;.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1629583119465" ID="ID_1548803670" MODIFIED="1629587102559" POSITION="right" TEXT="Constru&#xe7;&#xe3;o da Solu&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#00b439" CREATED="1629583168696" ID="ID_302164458" MODIFIED="1629587102569" TEXT="Diagrama de Atividades">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629584434313" ID="ID_74099037" MODIFIED="1629587102569" TEXT="&#xc9; um diagrama din&#xe2;mico que serve para ilustrar os procedimentos, processos de neg&#xf3;cio e fluxos de trabalho que afetam o sistema.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629583186441" ID="ID_1423523733" MODIFIED="1629587102574" TEXT="Diagrama de Sequ&#xea;ncias">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629584580991" ID="ID_1781727407" MODIFIED="1629587102574" TEXT="&#xc9; um diagrama din&#xe2;mico que enfatiza a troca de mensagens de objetos ao longo do tempo.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629584076998" ID="ID_1187170222" MODIFIED="1629587102574" TEXT=" Tipos de mensagens">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629584092133" ID="ID_808044985" MODIFIED="1629587102574" TEXT="S&#xed;ncronas">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1629584189147" ID="ID_121908576" MODIFIED="1629587102574" TEXT="S&#xed;ncrona: Quando voc&#xea; inicia um saque no caixa eletr&#xf4;nico o sistema primeiro envia uma mensagem de d&#xe9;bito para sua conta e precisa aguardar a confirma&#xe7;&#xe3;o pelo banco para ent&#xe3;o soltar o dinheiro, caso n&#xe3;o consiga soltar o dinheiro por sua vez envia uma mensagem para que seja gerado o extorno."/>
+<node COLOR="#111111" CREATED="1629584291413" ID="ID_1227631646" MODIFIED="1629587102579" TEXT="O objeto remetente interrompe seu processamento para esperar por resultados."/>
+</node>
+<node COLOR="#990000" CREATED="1629584131540" ID="ID_1746082707" MODIFIED="1629587102579" TEXT="Ass&#xed;ncronas">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1629584247427" ID="ID_773239822" MODIFIED="1629587102579" TEXT="Quando voc&#xea; envia uma resposta a um t&#xf3;pico em um f&#xf3;rum (ex. Alura) o sistema envia uma mensagem de e-mail assincrona para o autor do t&#xf3;pico, n&#xe3;o sendo necess&#xe1;rio aguardar a confirma&#xe7;&#xe3;o do envio da mensagem para continuar navegando."/>
+<node COLOR="#111111" CREATED="1629584323124" ID="ID_1168091479" MODIFIED="1629587102579" TEXT="O objeto remetente n&#xe3;o interrompe seu processamento para esperar por resultados."/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1629583357080" ID="ID_1763986099" MODIFIED="1629587102584" POSITION="left" TEXT="Valida&#xe7;&#xe3;o e Ajustes da Solu&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-4"/>
+<node COLOR="#00b439" CREATED="1629585882227" ID="ID_1697376910" MODIFIED="1629587102584" TEXT="Avalia&#xe7;&#xe3;o Integrada do Modelo L&#xf3;gico">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629585908415" ID="ID_539966224" MODIFIED="1629587102584" TEXT="Precisamos estar seguros e conscientes de que foram&#xa; atendidos os requisitos que foram passados inicialmente.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1629586058339" ID="ID_1101338609" MODIFIED="1629587102589" TEXT="Migra&#xe7;&#xe3;o para o Modelo F&#xed;sico">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1629586234117" ID="ID_1618830866" MODIFIED="1629587102594" TEXT="Tra&#xe7;ar uma estrat&#xe9;gia que nos auxilie na execu&#xe7;&#xe3;o desta etapa que envolver&#xe1;, por exemplo,&#xa; a intera&#xe7;&#xe3;o do analista que fez a modelagem com o l&#xed;der da equipe de desenvolvimento.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1629586385035" ID="ID_158790994" MODIFIED="1629587102594" TEXT="A demarca&#xe7;&#xe3;o de pacotes servem para dar um foco inicial na implementa&#xe7;&#xe3;o &#xa;e acelerar a entrega de uma vers&#xe3;o &#x201c;0&#x201d; da solu&#xe7;&#xe3;o para o cliente final.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1629586481966" ID="ID_190334033" MODIFIED="1629587102605" POSITION="right" STYLE="fork" TEXT="Benef&#xed;cios da UML">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="idea"/>
+<node COLOR="#00b439" CREATED="1629586516595" ID="ID_1295987239" MODIFIED="1629587102605" TEXT="Ser independente de linguagens de programa&#xe7;&#xe3;o e processos de desenvolvimento.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1629586528929" ID="ID_960914418" MODIFIED="1629587102605" TEXT="Fornecer aos usu&#xe1;rios uma linguagem de modelagem visual expressiva e pronta para uso visando o desenvolvimento de modelos.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1629586540307" ID="ID_907824614" MODIFIED="1629587102610" TEXT="Suporta conceitos de desenvolvimento de n&#xed;vel mais elevado tais como itera&#xe7;&#xe3;o entre usu&#xe1;rios, funcionalidades e componentes previstos.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1629586555437" ID="ID_623767009" MODIFIED="1629587102615" TEXT="Fornecer mecanismos de extensibilidade durante a modelagem.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
